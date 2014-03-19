c3802e72e92ec4cccc73b94534ecb79a19e4a319
Matthew Ford <matt@bitzesty.com>
2014-03-19 13:08:11 +0000

    Rabl caching
    This improves performance of the template generation of the JSON

--- config/initializers/rabl_config.rb
+++ config/initializers/rabl_config.rb
@@ -1,4 +1,8 @@
 Rabl.configure do |config|
   config.include_json_root = false
   config.include_child_root = false
+  config.cache_all_output = true
+  config.cache_sources = true 
+  config.cache_engine = Rabl::CacheEngine.new
+  config.perform_caching = true
 end
